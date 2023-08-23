68722e8f6a26bd3602f7997fd9153498934de026
Gleb Titov <reactdeveloper@vk.com>
2023-08-23 09:02:52 +0300

    auth

--- src/files/storage.ts
+++ src/files/storage.ts
@@ -1,5 +1,18 @@
 import { diskStorage } from 'multer';
 
+const generateId = () => {
+  Array(18)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+};
+
+const normalizeFileName = (req, file, callback) => {
+  const fileExName = file.originalname.split('.').pop();
+  callback(null, `${generateId()}.${fileExName}`);
+};
+
 export const fileStorage = diskStorage({
   destination: './uploads',
+  filename: normalizeFileName,
 });
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    default: 'Николавенко Стаон',
+  })
+  fullName: string;
+
+  @ApiProperty({
+    default: 'test@test.ru',
+  })
+  email: string;
+
+  @ApiProperty({
+    default: '123',
+  })
+  password: string;
+}
